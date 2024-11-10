3cc1342d61042b511ef9043ad5c6aa418be1b1d3
Artemko8 <153329908+Artemko8@users.noreply.github.com>
2024-11-10 02:41:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def get_weather():
         return jsonify({"error": "Invalid JSON response", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
